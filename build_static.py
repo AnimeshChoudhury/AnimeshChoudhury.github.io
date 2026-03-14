c2574fbf16022ae241316f6f73d87349e5703e03
AnimeshChoudhury <animesh.buddha@gmail.com>
2026-03-14 23:04:41 +0530

    modified build_static.py

--- build_static.py
+++ build_static.py
@@ -7,9 +7,10 @@ import os
 import shutil
 
 # --- CRITICAL CONFIGURATION FOR GITHUB PAGES ---
-app.config['FREEZER_RELATIVE_URLS'] = False
+app.config['FREEZER_RELATIVE_URLS'] = True
 app.config['FREEZER_DESTINATION'] = 'build'
 app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*', 'CNAME']
+app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
 
 freezer = Freezer(app)
 
@@ -19,6 +20,18 @@ def project_detail():
     projects_data = load_json('projects.json')
     for project in projects_data['projects']:
         yield {'project_id': project['id']}
+
+@freezer.register_generator
+def download_cv():
+    """Exclude file download routes from freezing"""
+    return
+    yield
+
+@freezer.register_generator
+def view_cv():
+    """Exclude file view routes from freezing"""
+    return
+    yield
 
 if __name__ == '__main__':
     # Clean build directory
